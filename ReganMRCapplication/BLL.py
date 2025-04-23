e0bba6154525a2e356c3cfcf632212b64a3009e6
Danny Regan <d.regan@columbia.edu>
2025-04-23 19:37:19 +0200

    Print total revenue by vessel.

--- ReganMRCapplication/BLL.py
+++ ReganMRCapplication/BLL.py
@@ -1,16 +1,30 @@
 import getpass
 
-from DAL import DBConnection
+from DAL import DBConnection, Vessel_DAL
+
+def printChart(data):
+  print("--------------------------------")
+  spaces = 13
+  for row in data:
+    print(row[0], " "*(spaces - len(row[0])), "| ", row[1])
+  print("--------------------------------")
 
 def main():
   pswd = getpass.getpass("Enter the database's password: ")
 
   print("Connecting to database...")
-  config = DBConnection(pswd)
-  connection, res = config.connect()
-  print(res) if connection else print(res)
+  db = DBConnection(pswd)
+  connection, res = db.connect()
+
+  if connection:
+    print(res)
+    input("Press enter to view the Total Revenue by Vessel: ")
+    view = Vessel_DAL.totalRevenueByVessel(connection)
+    printChart(view)
+  else: print(res)
+
 
   
-# =========================================
+# ===========================================
 if __name__ == '__main__':
   main()
--- ReganMRCapplication/DAL.py
+++ ReganMRCapplication/DAL.py
@@ -8,11 +8,40 @@ class DBConnection:
       'password': self.password,
       'database': 'mrc'
     }
+    self.connection = None
 
   def connect(self):
     try:
-      mydb = mysql.connector.connect(**self.config)
-      return mydb, 'Connection successful.'
+      self.connection = mysql.connector.connect(**self.config)
+      return self.connection, 'Connection successful.'
     except:
-      return None, "Connection failed."
+      return self.connection, "Connection failed."
+  
+  def cursor(self):
+    if self.connection:
+      return self.connection.cursor()
+    else:
+      return "Not connected to a database."
+  
+  def close(self):
+    if self.connection:
+      self.connection.close()
+      self.connection = None
 
+class Vessel_DAL:
+
+  def totalRevenueByVessel(connection):
+    cursor = connection.cursor()
+    query = "SELECT * FROM TotalRevenuebyVessel;"
+    cursor.execute(query)
+    result = cursor.fetchall()
+    cursor.close()
+    return result
+  
+  def getVesselID(connection, vesselName):
+    cursor = connection.cursor()
+    func = f"getVesselID({vesselName});"
+    cursor.execute(func)
+    result = cursor.fetchall()
+    cursor.close()
+    return result
